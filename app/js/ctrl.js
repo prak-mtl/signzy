8d741811e618c7056f2333f451608ff98d80c893
abhinav <developers.testone@gmail.com>
2017-12-03 19:34:39 +0530

    css change of body adding ajax call of json data

--- app/js/ctrl.js
+++ app/js/ctrl.js
@@ -1,17 +1,6 @@
-/* carousel.js */
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-		slideIndex = 1
-		}  
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+    $http.get("customers.php").then(function(response) {
+        $scope.myData = response.data.records;
+    });
+});
