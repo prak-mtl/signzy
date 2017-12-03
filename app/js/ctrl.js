6bf2c63a142844b9f6ec2879b46361bbfa4dc519
abhinav <developers.testone@gmail.com>
2017-12-03 22:59:23 +0530

    lot of things

--- app/js/ctrl.js
+++ app/js/ctrl.js
@@ -1,6 +1,49 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $http) {
-    $http.get("customers.php").then(function(response) {
-        $scope.myData = response.data.records;
-    });
+var app = angular.module('schoolApp', []);
+app.controller('schoolCtrl', function($scope, $http) {
+    $scope.gap = 5;
+    $scope.itemsPerPage = 5;
+    $scope.currentPage = 0;
+    $scope.pagedSchools = [];
+
+    $scope.groupToPages = function() {
+        $http.get("bangaloreSchools.json").then(function(response) {
+            $scope.schools = response.data;
+            for (var i = 0; i < $scope.schools.length; i++) {
+                if (i % $scope.itemsPerPage === 0) {
+                    $scope.pagedSchools[Math.floor(i / $scope.itemsPerPage)] = [$scope.schools[i]];
+                } else {
+                    $scope.pagedSchools[Math.floor(i / $scope.itemsPerPage)].push($scope.schools[i]);
+                }
+            }
+        });
+    };
+
+    $scope.range = function (size, start, end) {
+        var ret = [];
+        if (size < end) {
+            end = size;
+            start = size-$scope.gap;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        return ret;
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedSchools.length - 1) {
+            $scope.currentPage++;
+        }
+    };
+
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
 });
+
